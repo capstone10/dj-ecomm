ead211f7657699b74cf49e246222b2fece4a809f
Zayyad Musa <alias.co>
2021-12-16 10:47:19 +0100

    take

--- ecommerce/demo/urls.py
+++ ecommerce/demo/urls.py
@@ -8,5 +8,6 @@ app_name = "demo"
 urlpatterns = [
     path('', views.home, name="home"),
     path('categories/', views.category, name='category'),
-    path('product-by-category/<slug:category>/', views.product_by_category, name="product_by_category")
+    path('product-by-category/<slug:category>/', views.product_by_category, name="product_by_category"),
+    path('<slug:slug>/', views.product_detail, name='product_detail'),
 ]
--- ecommerce/demo/views.py
+++ ecommerce/demo/views.py
@@ -9,14 +9,24 @@ def home(request):
 def category(request):
     data = models.Category.objects.all()
 
-    print(models.Category.objects.all().query)
+    # print(models.Category.objects.all().query)
 
     return render(request, "category.html",{"data":data})
 
 
 def product_by_category(request, category):
-    print(category)
-    product = models.Product.objects.filter(category__name=category).filter(product__is_default=True).values("id","name","slug","category__name","product__store_price")
+    # print(category)
+    product = models.Product.objects.filter(category__name=category).values
+    ("id","name","slug","category__name","product_inventory__store_price")
+    
     print(product)
 
     return render(request, "product_by_category.html",{"product":product})
+
+
+def product_detail(request, slug):
+    data = models.ProductInventory.objects.filter(product__slug=slug).values(
+    "id","sku","product__name","store_price","product_inventory__units")
+
+
+    return render(request, "product_detail.html", {"data":data})
